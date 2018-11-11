130be98d960df5a0d7f75b66ee6748f87c425d44
moi <wxopwx@gmail.com>
2018-11-11 08:13:31 +0100

    load custom config file (if ever it exists) to override modem_ip or statfile

--- getstats.sh
+++ getstats.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+SCRIPTDIR=`dirname $0`
+CONFIG=$SCRIPTDIR/config
+[ -f $CONFIG ] &&  . $CONFIG
+
 statsfile=${statsfile:-/var/lib/munin/plugin-state/huawei-hg612-munin-output.txt}
+modem_ip=${modem_ip:-"192.168.1.1"}
 
 expect `dirname $0`/huawei.expect  | tee | tr -d "\015"  > $statsfile
